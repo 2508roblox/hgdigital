da9a5d5a556a1654973175f67f0e63bfd8d435dc
2508roblox <trangiangzxc@gmail.com>
2025-01-19 17:08:47 +0700

    update

--- resources/views/livewire/template-detail.blade.php
+++ resources/views/livewire/template-detail.blade.php
@@ -42,7 +42,7 @@
         <div class="body_wrap">
 
             <!-- header start -->
-            <livewire:header-secondary />
+            @livewire('inc.header-blog')
 
             <!-- header end -->
 
@@ -457,137 +457,14 @@
                 <!-- service-details-content end  -->
 
                 <!-- cta section start  -->
-                <section class="cta ">
-                    <div class="container ">
-                        <div class="cta-wrap ">
-                            <div class="cta-inner ul_li_between ">
-                                <div class="xb-item--holder wow fadeInLeft " data-wow-delay="100ms "
-                                    data-wow-duration="600ms ">
-                                    <h2 class="xb-item--title ">Stop wasting money on bad SEO.</h2>
-                                    <span class="xb-item--content ">Book a free consultation for the SEO results you
-                                        need.</span>
-                                    <div class="xb-btn mt-45 ">
-                                        <a href="/contact " class="thm-btn thm-btn--aso thm-btn--aso_white ">Book a
-                                            free consultation</a>
-                                    </div>
-                                </div>
-                                <div class="cta-right_img wow fadeInRight " data-wow-delay="150ms "
-                                    data-wow-duration="600ms ">
-                                    <img class="updown " src="{{ asset('assets/img/cta/clip-bord.png ') }}"
-                                        alt=" ">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                @livewire('inc.footer-cta')
                 <!-- cta section end  -->
 
             </main>
             <!-- main area end  -->
 
             <!-- footer strt -->
-            <footer class="footer footer-style-two pt-200 bg_img pos-rel " data-bg-color="#0c111d ">
-                <div class="container ">
-                    <div class="xb-footer pt-120 ">
-                        <div class="footer-info ul_li_between ">
-                            <div class="info-item ul_li ">
-                                <div class="xb-item--icon ">
-                                    <span><img src="{{ asset('assets/img/icon/sms-white.svg ') }}"
-                                            alt=" "></span>
-                                </div>
-                                <div class="xb-item--holder ">
-                                    <p class="xb-item--content ">Write to us</p>
-                                    <h4 class="xb-item--title ">innomax@gmail.com</h4>
-                                </div>
-                            </div>
-                            <div class="info-item ul_li ">
-                                <div class="xb-item--icon clr-blue ">
-                                    <span><img src="{{ asset('assets/img/icon/call-white.svg ') }}"
-                                            alt=" "></span>
-                                </div>
-                                <div class="xb-item--holder ">
-                                    <p class="xb-item--content ">Call Us (USA)</p>
-                                    <h4 class="xb-item--title ">+(1) 1230 452 8597</h4>
-                                </div>
-                            </div>
-                            <div class="info-item ul_li ">
-                                <div class="xb-item--icon clr-sky ">
-                                    <span><img src="{{ asset('assets/img/icon/location.svg ') }}"
-                                            alt=" "></span>
-                                </div>
-                                <div class="xb-item--holder ">
-                                    <p class="xb-item--content ">Our Office</p>
-                                    <h4 class="xb-item--title ">Waterloo,Park,Australia</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="footer-inner mt-70 mb-100 ul_li_between align-items-start ">
-                            <div class="sa-newslatter footer-widget ">
-                                <span class="xb-item--sub-title ">Newsletter</span>
-                                <p class="xb-item--content clr-white ">Sign up to techco weekly newsletter to get the
-                                    latest updates.
-                                </p>
-                                <div class="xb-item--input_field pos-rel ">
-                                    <input type="email " name="gmail " id="text6 "
-                                        placeholder="enter your email ">
-                                    <div class="img "><img src="{{ asset('assets/img/icon/sms02.svg ') }}"
-                                            alt=" "></div>
-                                    <button type="submit " class="xb-item--btn "><i
-                                            class="fas fa-paper-plane "></i></button>
-                                </div>
-                                <span class="xb-item--text ">By continuing, you agree to innomax <a
-                                        href="#! ">Terms</a> of
-                                    Use and <a href="#! ">Privacy Policy</a></span>
-                            </div>
-                            <div class="footer-widget ">
-                                <span class="xb-item--sub-title ">Company</span>
-                                <ul class="xb-item--holder list-unstyled ">
-                                    <li class="xb-item--list "><a href="#! ">About us</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Contact us</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Price table</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Our blog</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Team member</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Our Cases</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-widget ">
-                                <span class="xb-item--sub-title ">Our Services</span>
-                                <ul class="xb-item--holder list-unstyled ">
-                                    <li class="xb-item--list "><a href="#! ">SEO copywriting</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Managed SEO</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Small business SEO</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Monthly SEO</a></li>
-                                    <li class="xb-item--list "><a href="#! "> National SEO</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Technical SEO</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-widget ">
-                                <span class="xb-item--sub-title ">Our Industries</span>
-                                <ul class="xb-item--holder list-unstyled ">
-                                    <li class="xb-item--list "><a href="#! ">Healthcare</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Lawyers</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Real estate</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Insurance</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Crypto</a></li>
-                                    <li class="xb-item--list "><a href="#! ">Automotive</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="footer-copyright mt-70 ul_li_between ">
-                            <p class="copyright mt-20 ">Copyright © 2024 <a href="/ ">innomax</a>. All rights
-                                reserved.
-                            </p>
-                            <ul class="footer-link ul_li mt-20 ">
-                                <li><span>Follow us :</span></li>
-                                <li><a href="#! "><i class="fab fa-twitter "></i></a></li>
-                                <li><a href="#! "><i class="fab fa-facebook-f "></i></a></li>
-                                <li><a href="#! "><i class="fab fa-linkedin-in "></i></a></li>
-                                <li><a href="#! "><i class="fab fa-youtube "></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            @livewire('inc.footer-blog')
             <!-- footer end -->
 
         </div>
--- resources/views/livewire/template-list.blade.php
+++ resources/views/livewire/template-list.blade.php
@@ -379,7 +379,7 @@
                                                                 <!-- Phần giá -->
                                                                 <div class="price-wrap">
                                                                     <span
-                                                                        class="current-price">{{ number_format($template->discount_price, 0, ',', '.') }}₫</span>
+                                                                        class="current-price">{{ number_format($template->discounted_price, 0, ',', '.') }}₫</span>
                                                                     <!-- Giá khuyến mãi -->
                                                                     @if ($template->original_price > $template->discount_price)
                                                                         <span
@@ -502,111 +502,14 @@
 
 
                 <!-- cta section start  -->
-               @livewire('inc.footer-cta')
+                @livewire('inc.footer-cta')
                 <!-- cta section end  -->
 
             </main>
             <!-- main area end  -->
 
             <!-- footer strt -->
-            <footer class="footer footer-style-two pt-200 bg_img pos-rel" data-bg-color="#0c111d"
-                style="background-color: rgb(12, 17, 29);">
-                <div class="container">
-                    <div class="xb-footer pt-120">
-                        <div class="footer-info ul_li_between">
-                            <div class="info-item ul_li">
-                                <div class="xb-item--icon">
-                                    <span><img src="assets/img/icon/sms-white.svg" alt=""></span>
-                                </div>
-                                <div class="xb-item--holder">
-                                    <p class="xb-item--content">Write to us</p>
-                                    <h4 class="xb-item--title">innomax@gmail.com</h4>
-                                </div>
-                            </div>
-                            <div class="info-item ul_li">
-                                <div class="xb-item--icon clr-blue">
-                                    <span><img src="assets/img/icon/call-white.svg" alt=""></span>
-                                </div>
-                                <div class="xb-item--holder">
-                                    <p class="xb-item--content">Call Us (USA)</p>
-                                    <h4 class="xb-item--title">+(1) 1230 452 8597</h4>
-                                </div>
-                            </div>
-                            <div class="info-item ul_li">
-                                <div class="xb-item--icon clr-sky">
-                                    <span><img src="assets/img/icon/location.svg" alt=""></span>
-                                </div>
-                                <div class="xb-item--holder">
-                                    <p class="xb-item--content">Our Office</p>
-                                    <h4 class="xb-item--title">Waterloo,Park,Australia</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="footer-inner mt-70 mb-100 ul_li_between align-items-start">
-                            <div class="sa-newslatter footer-widget">
-                                <span class="xb-item--sub-title">Newsletter</span>
-                                <p class="xb-item--content clr-white">Sign up to techco weekly newsletter to get the
-                                    latest
-                                    updates.</p>
-                                <div class="xb-item--input_field pos-rel">
-                                    <input type="email" name="gmail" id="text6"
-                                        placeholder="enter your email">
-                                    <div class="img"><img src="assets/img/icon/sms02.svg" alt=""></div>
-                                    <button type="submit" class="xb-item--btn"><i
-                                            class="fas fa-paper-plane"></i></button>
-                                </div>
-                                <span class="xb-item--text">By continuing, you agree to innomax <a
-                                        href="#!">Terms</a> of
-                                    Use and <a href="#!">Privacy Policy</a></span>
-                            </div>
-                            <div class="footer-widget">
-                                <span class="xb-item--sub-title">Company</span>
-                                <ul class="xb-item--holder list-unstyled">
-                                    <li class="xb-item--list"><a href="#!">About us</a></li>
-                                    <li class="xb-item--list"><a href="#!">Contact us</a></li>
-                                    <li class="xb-item--list"><a href="#!">Price table</a></li>
-                                    <li class="xb-item--list"><a href="#!">Our blog</a></li>
-                                    <li class="xb-item--list"><a href="#!">Team member</a></li>
-                                    <li class="xb-item--list"><a href="#!">Our Cases</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-widget">
-                                <span class="xb-item--sub-title">Our Services</span>
-                                <ul class="xb-item--holder list-unstyled">
-                                    <li class="xb-item--list"><a href="#!">SEO copywriting</a></li>
-                                    <li class="xb-item--list"><a href="#!">Managed SEO</a></li>
-                                    <li class="xb-item--list"><a href="#!">Small business SEO</a></li>
-                                    <li class="xb-item--list"><a href="#!">Monthly SEO</a></li>
-                                    <li class="xb-item--list"><a href="#!"> National SEO</a></li>
-                                    <li class="xb-item--list"><a href="#!">Technical SEO</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-widget">
-                                <span class="xb-item--sub-title">Our Industries</span>
-                                <ul class="xb-item--holder list-unstyled">
-                                    <li class="xb-item--list"><a href="#!">Healthcare</a></li>
-                                    <li class="xb-item--list"><a href="#!">Lawyers</a></li>
-                                    <li class="xb-item--list"><a href="#!">Real estate</a></li>
-                                    <li class="xb-item--list"><a href="#!">Insurance</a></li>
-                                    <li class="xb-item--list"><a href="#!">Crypto</a></li>
-                                    <li class="xb-item--list"><a href="#!">Automotive</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="footer-copyright mt-70 ul_li_between">
-                            <p class="copyright mt-20">Copyright © 2024 <a href="index.html">innomax</a>. All rights
-                                reserved.</p>
-                            <ul class="footer-link ul_li mt-20">
-                                <li><span>Follow us :</span></li>
-                                <li><a href="#!"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#!"><i class="fab fa-linkedin-in"></i></a></li>
-                                <li><a href="#!"><i class="fab fa-youtube"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            @livewire('inc.footer-blog')
             <!-- footer end -->
 
         </div>
